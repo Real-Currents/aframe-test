be33ad27c92ce25ee2927639f33abbb2b60c41c6
John Hall <john.hall@ruralinnovation.us>
2024-01-04 13:34:31 -0500

    hide download and print options

--- src/@cori-risi/bead/App.tsx
+++ src/@cori-risi/bead/App.tsx
@@ -13,10 +13,8 @@ import {
 } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import './App.css';
-import amplifyLogo from './assets/amplify.svg';
-import reactLogo from './assets/react.svg';
-import reduxLogo from './assets/redux.svg';
-import viteLogo from './assets/vite.svg';
+import ApplicationMenu from "./components/ApplicationMenu";
+import './components/styles/ApplicationMenu.scss';
 
 import User from '../models/User';
 import {
@@ -58,7 +56,7 @@ function App ({ content, user }: { content: () => HTMLElement, user: Promise<Use
         // console.log(aws_config);
     }());
 
-    const allowMenuToBeClosed = true;
+    const allowMenuToBeClosed = false;
     const [ controlPanelOpen, setControlPanelOpen ] = useState<boolean>(!allowMenuToBeClosed);
     const [ showMenuButton, setShowMenuButton ] = useState<boolean>(!!allowMenuToBeClosed);
 
@@ -183,6 +181,11 @@ function App ({ content, user }: { content: () => HTMLElement, user: Promise<Use
     );
 }
 
+// export default App;
+export default withAuthenticator(App, {
+    loginMechanisms: ['username']
+});
+
 function Counter () {
     const count = useSelector(selectCount);
     const dispatch = useDispatch();
@@ -200,139 +203,6 @@ function Counter () {
                     onClick={() => (dispatch as Function)(decrement())} >
                 -
             </Button>
-        </div>
-    );
-}
-
-function ApplicationMenu () {
-
-    // const userState: User = useSelector(selectUser);
-
-    function showPrintOptions() {
-        try {
-            const print_config_form = document.getElementById("print-config")!;
-            print_config_form.className = "show";
-            const print_config_button = document.getElementById("print-config-btn")!;
-            print_config_button.className = "amplify-button hide";
-        } catch (e: unknown) {}
-    }
-
-    return (
-        <div id={"application-menu"} style={{ minWidth: "254px" }}>
-
-            <div id={"print-exec"} className="row">
-                <Button type="submit"  id={"print-config-btn"}
-                        className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"}
-                        onClick={showPrintOptions}>
-                    Show print options</Button>
-                {/* */}
-                {/* Print config form */}
-                {/* */}
-                <form id={"print-config"}>
-                    <fieldset id="config-fields">
-                        <div>
-                            <div className="col-sm-4">
-                                <div className="form-group">
-                                    <label>Unit</label><br />
-                                    <label className="radio-inline">
-                                        <input type="radio" name="unitOptions" value="in" id="inUnit" checked readOnly /> Inch
-                                    </label>
-                                    <label className="radio-inline">
-                                        <input type="radio" name="unitOptions" value="mm" id="mmUnit" readOnly /> Millimeter
-                                    </label>
-                                </div>
-                            </div>
-                            <div className="col-sm-3">
-                                <div className="form-group">
-                                    <label>Output format</label><br />
-                                    <label className="radio-inline">
-                                        <input type="radio" name="outputOptions" value="png" checked readOnly /> PNG
-                                    </label>
-                                    <label className="radio-inline">
-                                        <input type="radio" name="outputOptions" value="pdf" readOnly /> PDF
-                                    </label>
-                                </div>
-                            </div>
-                            <div className="col-sm-5">
-                                <div className="form-group">
-                                    <label htmlFor="styleSelect">Map style</label>
-                                    <select id="styleSelect" className="form-control">
-                                        <option value="mapbox://styles/mapbox/light-v9">Mapbox Light</option>
-                                        <option value="mapbox://styles/mapbox/streets-v10">Mapbox Streets</option>
-                                        <option value="https://tiles.stadiamaps.com/styles/alidade_smooth.json">Stadia Maps
-                                            Alidade Smooth
-                                        </option>
-                                        <option value="https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json">Stadia
-                                            Maps Alidade Smooth Dark
-                                        </option>
-                                        <option value="https://tiles.stadiamaps.com/styles/outdoors.json">Stadia Maps
-                                            Outdoors
-                                        </option>
-                                        <option value="https://tiles.stadiamaps.com/styles/osm_bright.json">Stadia Maps OSM
-                                            Bright
-                                        </option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="col-sm-2">
-                                <div className="form-group" id="widthGroup">
-                                    <label htmlFor="widthInput">Width</label>
-                                    <input type="text" className="form-control" id="widthInput" autoComplete="off"
-                                           readOnly value="8" />
-                                </div>
-                            </div>
-                            <div className="col-sm-2">
-                                <div className="form-group" id="heightGroup">
-                                    <label htmlFor="heightInput">Height</label>
-                                    <input type="text" className="form-control" id="heightInput" autoComplete="off"
-                                           readOnly value="6" />
-                                </div>
-                            </div>
-                            <div className="col-sm-3">
-                                <div className="form-group" id="dpiGroup">
-                                    <label htmlFor="dpiInput">DPI</label>
-                                    <input type="text" className="form-control" id="dpiInput" autoComplete="off"
-                                           readOnly value="300" />
-                                </div>
-                            </div>
-                            <div className="col-sm-5">
-                                <div>
-                                    <div className="col-sm-4">
-                                        <div className="form-group" id="latGroup">
-                                            <label htmlFor="latInput">Latitude</label>
-                                            <input type="text" className="form-control" id="latInput" autoComplete="off"
-                                                   readOnly value="" />
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-4">
-                                        <div className="form-group" id="lonGroup">
-                                            <label htmlFor="lonInput">Longitude</label>
-                                            <input type="text" className="form-control" id="lonInput" autoComplete="off"
-                                                   readOnly value="" />
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-4">
-                                        <div className="form-group" id="zoomGroup">
-                                            <label htmlFor="zoomInput">Zoom</label>
-                                            <input type="text" className="form-control" id="zoomInput" autoComplete="off"
-                                                   readOnly value="" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </fieldset>
-                </form>
-
-                <button type="submit" className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"} id={"generate-btn"}>Print report</button>
-                <div id='spinner' />
-            </div>
-
-            <a id={"data-download-link"}>
-                <button type="submit" className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"} id={"data-download-btn"}>Download data</button>
-            </a>
         </div>
     );
 }
@@ -440,7 +310,7 @@ function ControlPanel (props: {
                  }}>
 
                 {(userState !== null) ? (
-                    <h5>{ getUserLabel(userState)}</h5>
+                    <div><span className={"form-label"}>User</span>: { getUserLabel(userState) }</div>
                 ) : (
                     <div style={{ display: "none" }} />
                 )}
@@ -466,8 +336,3 @@ function ControlPanel (props: {
 function SignOutButton ({ signOut }: { signOut: Function }) {
     return <Button className={"amplify-sign-out"} title="Sign Out" onClick={() => { signOut(); }}>Sign Out</Button>;
 }
-
-// export default App;
-export default withAuthenticator(App, {
-    loginMechanisms: ['username']
-});
