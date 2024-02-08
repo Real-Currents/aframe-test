5033b9f7020ec6a9602c9cd5c3bcfda15bbd1fc0
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2024-02-08 15:08:20 -0700

    navbar scaffolding

--- src/@cori-risi/bead/components/Navbar.tsx
+++ src/@cori-risi/bead/components/Navbar.tsx
@@ -5,8 +5,13 @@ export default function Navbar({ text }) {
 
     return ( 
     	<>
-    		<div className={style['navbar']}>
-          <img src='/Full-Logo_CORI_Dark-Teal.svg'/>
+    		<div className={style['navbar-container']}>
+          <div className={style['navbar']}>
+            <img className={style['logo']} src='/Full-Logo_CORI_Cream.svg'/>
+            <a className={style['menu']}>
+              <img src="/menu.svg" />
+            </a>
+          </div>
         </div>
       </>
     );
