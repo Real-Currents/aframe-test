5d88cc64db2787ea998eab80f852437a7d41ec07
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2024-02-20 10:59:44 -0700

    using better CORI custom basemap

--- src/@cori-risi/bead/components/GlMap.tsx
+++ src/@cori-risi/bead/components/GlMap.tsx
@@ -354,7 +354,7 @@ const GlMap: React.FC < GlMapProps > = ({
                       longitude: longitude,
                       zoom: zoom
                     }}
-                    mapStyle="mapbox://styles/mapbox/light-v9"
+                    mapStyle="mapbox://styles/ruralinno/cl010e7b7001p15pe3l0306hv"
                     mapboxAccessToken={mapboxToken}
                     interactiveLayerIds={
                         (bead_dev.layers !== null && bead_dev.layers[0].hasOwnProperty('id') ) ? [
@@ -365,12 +365,6 @@ const GlMap: React.FC < GlMapProps > = ({
                     onMove={onMove}
                     onClick={makeOnClick(apiContext.token!.toString())}
                 >
-
-                    <Source id={"mapbox-terrain"} type={"vector"} url={"mapbox://mapbox.mapbox-terrain-v2"} >
-                        <Layer {...contourStyle} >
-                        </Layer>
-                    </Source>
-
                     <Source {...bead_dev.sources[0]} >
                         <Layer
                           {...layerAttributes}
